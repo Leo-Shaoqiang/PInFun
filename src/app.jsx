e451bcdfb69857ae2fd79c06ab95639c9c36b7b9
MooozzZ <1362610792@qq.com>
2020-06-05 18:31:36 +0800

    test 规范

--- src/app.jsx
+++ src/app.jsx
@@ -9,7 +9,7 @@ import './app.scss'
 // 取消以下注释：
 // if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
 //   require('nerv-devtools')
-// }
+// }  
 // 
 const store = configStore()
 
